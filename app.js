ef7e83062e069b15d737d50e4ef35881bcac90a8
Chris Shay <christopher.jackson.shay@gmail.com>
2017-04-12 14:35:03 -0400

    Fix bugs and continue practice

--- app.js
+++ app.js
@@ -1,164 +1,134 @@
-(function () {
-  var app = angular.module('store', []);
+(function() {
+  var app = angular.module('gemStore', []);
 
-  app.controller('StoreController', function(){
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(imageNumber){
+      this.current = imageNumber || 0;
+    };
+  });
+
+  app.controller('StoreController', function() {
     this.products = gems;
   });
 
-  app.controller('PanelController', function() {
+  app.controller("TabController", function() {
     this.tab = 1;
 
-    this.selectTab = function(setTab) {
-      this.tab = setTab;
+    this.isSet = function(checkTab) {
+      return this.tab === checkTab;
     };
 
-    this.isSelected = function(checkTab){
-      return this.tab === checkTab;
+    this.setTab = function(setTab) {
+      this.tab = setTab;
     };
   });
 
-  app.controller('ReviewController', function() {
+  app.controller("ReviewController", function(){
+
     this.review = {};
 
-    this.addReview = function(product) {
+    this.addReview = function(product){
       product.reviews.push(this.review);
       this.review = {};
     };
+
   });
 
+  app.directive("productDescription", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-description.html"
+    };
+  });
 
+  app.directive("productReviews", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-reviews.html"
+    };
+  });
+
+  app.directive("productSpecs", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-specs.html"
+    };
+  });
 
 
 
   var gems = [
     {
-      name: 'Awesome Multi-touch keyboard',
-      price: 2,
-      description: '. . .',
+      name: 'Azurite',
+      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+      shine: 8,
+      price: 110.50,
+      rarity: 7,
+      color: '#CCC',
+      faces: 14,
       images: [
-        {
-          full: 'dodecahedron-01-full.jpg',
-          thumb: 'dodecahedron-01-thumb.jpg'
-        },
-        {
-          full: 'dodecahedron-02-full.jpg',
-          thumb: 'dodecahedron-02-thumb.jpg'
-        },
-        {
-          full: 'dodecahedron-03-full.jpg',
-          thumb: 'dodecahedron-03-thumb.jpg'
-        }
+        "images/gem-02.gif",
+        "images/gem-05.gif",
+        "images/gem-09.gif"
       ],
-      reviews: [
-        {
-          stars: 5,
-          body: "I love this!",
-          author: "joe@joe"
-        },
-        {
-          stars: 1,
-          body: "this product sucks",
-          author: "tim@hater"
-        }
-      ],
-      canPurchase: true,
-      soldOut: true
-    },
-    {
-      name: "Brom",
-      price: 9.1,
-      description: ". . .",
-      reviews: [
-        {
-          stars: 5,
-          body: "I love this!",
-          author: "joe@joe"
-        },
-        {
-          stars: 1,
-          body: "this product sucks",
-          author: "tim@hater"
-        }
-      ],
+      reviews: [{
+        stars: 5,
+        body: "I love this gem!",
+        author: "joe@example.org"
+      }, {
+        stars: 1,
+        body: "This gem sucks.",
+        author: "tim@example.org"
+      }]
+    }, {
+      name: 'Bloodstone',
+      description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+      shine: 9,
+      price: 22.90,
+      rarity: 6,
+      color: '#EEE',
+      faces: 12,
       images: [
-        {
-          full: 'dodecahedron-01-full.jpg',
-          thumb: 'dodecahedron-01-thumb.jpg'
-        },
-        {
-          full: 'dodecahedron-02-full.jpg',
-          thumb: 'dodecahedron-02-thumb.jpg'
-        },
-        {
-          full: 'dodecahedron-03-full.jpg',
-          thumb: 'dodecahedron-03-thumb.jpg'
-        }
+        "images/gem-01.gif",
+        "images/gem-03.gif",
+        "images/gem-04.gif"
       ],
-      canPurchase: true
-    },
-    {
-      name: "Trom",
-      price: 590,
-      description: ". . .",
-      reviews: [
-        {
-          stars: 5,
-          body: "I love this!",
-          author: "joe@joe"
-        },
-        {
+      reviews: [{
+        stars: 3,
+        body: "I think this gem was just OK, could honestly use more shine, IMO.",
+        author: "JimmyDean@example.org"
+      }, {
+        stars: 4,
+        body: "Any gem with 12 faces is for me!",
+        author: "gemsRock@example.org"
+      }]
+      }, {
+        name: 'Zircon',
+        description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+        shine: 70,
+        price: 1100,
+        rarity: 2,
+        color: '#000',
+        faces: 6,
+        images: [
+          "images/gem-06.gif",
+          "images/gem-07.gif",
+          "images/gem-08.gif"
+        ],
+        reviews: [{
           stars: 1,
-          body: "this product sucks",
-          author: "tim@hater"
-        }
-      ],
-      images: [
-        {
-          full: 'dodecahedron-01-full.jpg',
-          thumb: 'dodecahedron-01-thumb.jpg'
-        },
-        {
-          full: 'dodecahedron-02-full.jpg',
-          thumb: 'dodecahedron-02-thumb.jpg'
-        },
-        {
-          full: 'dodecahedron-03-full.jpg',
-          thumb: 'dodecahedron-03-thumb.jpg'
-        }
-      ],
-      canPurchase: true
-    },
-    {
-      name: "Grom",
-      price: 25,
-      description: ". . .",
-      reviews: [
-        {
-          stars: 5,
-          body: "I love this!",
-          author: "joe@joe"
-        },
-        {
+          body: "This gem is WAY too expensive for its rarity value.",
+          author: "turtleguyy@example.org"
+        }, {
           stars: 1,
-          body: "this product sucks",
-          author: "tim@hater"
-        }
-      ],
-      images: [
-        {
-          full: 'dodecahedron-01-full.jpg',
-          thumb: 'dodecahedron-01-thumb.jpg'
-        },
-        {
-          full: 'dodecahedron-02-full.jpg',
-          thumb: 'dodecahedron-02-thumb.jpg'
-        },
-        {
-          full: 'dodecahedron-03-full.jpg',
-          thumb: 'dodecahedron-03-thumb.jpg'
-        }
-      ],
-      canPurchase: false
+          body: "BBW: High Shine != High Quality.",
+          author: "LouisW407@example.org"
+        }, {
+          stars: 1,
+          body: "Don't waste your rubles!",
+          author: "nat@example.org"
+        }]
     }
   ];
 })();
